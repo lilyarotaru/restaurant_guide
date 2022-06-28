3d053d37203eb4173ebc5fa4b6f84d1abd9b530d
Lilya Rotaru <rotaru.lulu@gmail.com>
2022-06-28 10:54:20 +0300

    Start project

---
+++ src/main/java/com/example/restaurant_guide/RestaurantGuideApplication.java
@@ -0,0 +1,13 @@
+package com.example.restaurant_guide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantGuideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantGuideApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/restaurant_guide/RestaurantGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restaurant_guide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantGuideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
